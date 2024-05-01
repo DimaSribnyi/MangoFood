9a159187de3a8be16b97104706f63465329e2454
DimaSribnyi <dondimon18@gmail.com>
2024-05-01 11:03:28 +0300

    Revert "Message bus to git ignore"
    This reverts commit 0d1467525e9afd090965ac97ff0bc25924071553.

---
+++ MangoFood.MessageBus/MessageBusService.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MangoFood.MessageBus
+{
+    public class MessageBusService : IMessageBusService
+    {
+        private string connectionString = "Endpoint=sb://mangofoodweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=PXZq/qtBZOMzhSRj+aP96NbVTUvvRoY1V+ASbC68d3g=";
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+        }
+    }
+}
